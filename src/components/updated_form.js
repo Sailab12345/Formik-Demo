660b357c9e27487666d0f1794eb1f6acfd2d529d
Sailab12345 <sailabbasak1998@gmail.com>
2021-04-19 22:14:07 +0530

    useformik done

--- src/components/updated_form.js
+++ src/components/updated_form.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 
 const initialValues = {
@@ -13,21 +13,6 @@ const onSubmit = (values) => {
   console.log("Form Data:", values);
 };
 
-// const validation = (values) => {
-//   const errors = {};
-//   debugger;
-//   if (!values.name) {
-//     errors.name = "Required";
-//   }
-//   if (!values.email) {
-//     errors.email = "Required";
-//   }
-//   if (!values.channel) {
-//     errors.channel = "Required";
-//   }
-//   return errors;
-// };
-
 const validationSchema = Yup.object({
   name: Yup.string().required("Required!!"),
   email: Yup.string().required("Required!!"),
@@ -36,26 +21,14 @@ const validationSchema = Yup.object({
 
 const Form = () => {
   debugger;
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema
-  });
-  // values = "";
+  <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit} />;
   return (
     <>
-      <form onSubmit={formik.handleSubmit}>
+      <Form>
         <div className="form-control">
           <div className="ui label">Name</div>
           <div className="ui input">
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              onBlur={formik.onBlur}
-            />
+            <Field type="text" name="name" id="name" />
           </div>
           {formik.touched.name && formik.errors.name ? (
             <div className="error">{formik.errors.name}</div>
@@ -64,14 +37,7 @@ const Form = () => {
         <div className="form-control">
           <div className="ui label">Email</div>
           <div className="ui input">
-            <input
-              type="text"
-              name="email"
-              id="email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              onBlur={formik.onBlur}
-            />
+            <Field type="text" name="email" id="email" />
           </div>
           {formik.touched.email && formik.errors.email ? (
             <div className="error">{formik.errors.email}</div>
@@ -80,21 +46,14 @@ const Form = () => {
         <div className="form-control">
           <div className="ui label">Channel</div>
           <div className="ui input">
-            <input
-              type="text"
-              name="channel"
-              id="channel"
-              value={formik.values.channel}
-              onChange={formik.handleChange}
-              onBlur={formik.onBlur}
-            />
+            <Field type="text" name="channel" id="channel" />
           </div>
           {formik.touched.channel && formik.errors.channel ? (
             <div className="error">{formik.errors.channel}</div>
           ) : null}
         </div>
         <button type="submit">SUBMIT</button>
-      </form>
+      </Form>
     </>
   );
 };
