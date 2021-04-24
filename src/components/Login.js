d2a1956587b3cae9051a74b0092eea9e27d7f086
Sailab12345 <sailabbasak1998@gmail.com>
2021-04-24 20:55:56 +0530

    Validation updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,27 @@
 import { Formik, Form, ErrorMessage, Field } from "formik";
 import * as Yup from "yup";
+import { number } from "yup/lib/locale";
 const Login = () => {
   const initialvalues = {
     email: "",
     password: ""
   };
   const onSubmit = (values) => {
-    debugger;
+    // debugger;
     const { email, password } = values;
+    if (email === "sailab98@gmail.com" && password === "12345") {
+      console.log("Login Succesful");
+    }
     console.log(values);
   };
+  // const handleSubmit = () => {
+  //   debugger;
+  //   return <div>"Login Successful"</div>;
+  // };
   const validationSchema = Yup.object({
     email: Yup.string().email("Invalid email").required("Required"),
-    password: Yup.string().required("Required")
+    password: Yup.string().required("Required"),
+    phone: Yup.number().required("Required")
   });
 
   return (
@@ -23,19 +32,26 @@ const Login = () => {
           <label>Email</label>
           <div>
             <Field name="email" type="email" />
-            <div>
+            <div style={{ marginBottom: "20px" }}>
               <ErrorMessage name="email" />
             </div>
           </div>
-          <label>Password</label>
+          <label>Phone no.</label>
           <div>
+            <Field name="phone" />
+            <div style={{ marginBottom: "20px" }}>
+              <ErrorMessage name="phone">
+                {(msg) => (
+                  <div>{msg === "Required" ? <div>"Required"</div> : "Phone must be number"}</div>
+                )}
+              </ErrorMessage>
+            </div>
+          </div>
+          <label>Password</label>
+          <div style={{ marginBottom: "20px" }}>
             <Field name="password" type="password" />
             <div>
-              <ErrorMessage
-                component="div"
-                className="info placeholder text-size-ten text-color-red"
-                name="password"
-              />
+              <ErrorMessage name="password" />
             </div>
           </div>
           <button type="submit">Submit</button>
