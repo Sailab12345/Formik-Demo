5433844f65b083dcece75bc4511772fe2a20a620
Sailab12345 <sailabbasak1998@gmail.com>
2021-04-26 23:41:07 +0530

    select done

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import { Formik, Form, formik, ErrorMessage, Field } from "formik";
 import { useState } from "react";
 import { useHistory } from "react-router";
 import * as Yup from "yup";
+import Dropdown from "./Dropdown";
 import { number } from "yup/lib/locale";
 const Login = () => {
   const [message, setMessage] = useState([""]);
@@ -35,7 +36,8 @@ const Login = () => {
   const validationSchema = Yup.object({
     email: Yup.string().email("Invalid email").required("Required"),
     password: Yup.string().required("Required"),
-    phone: Yup.number().required("Required")
+    phone: Yup.number().required("Required"),
+    select: Yup.string().required("Required!")
   });
 
   // const validate = (value) => {
@@ -50,38 +52,47 @@ const Login = () => {
         {({ isSubmitting }) => {
           return (
             <Form>
-              <h1>Login</h1>
-              <label>Email</label>
-              <div>
-                <Field name="email" type="email" />
-                <div style={{ marginBottom: "20px" }}>
-                  <ErrorMessage name="email" />
-                </div>
-              </div>
-              <label>Phone no.</label>
-              <div>
-                <Field name="phone" />
-                <div style={{ marginBottom: "20px" }}>
-                  <ErrorMessage name="phone">
-                    {(msg) => (
-                      <div>
-                        {msg === "Required" ? <div>"Required"</div> : "Phone must be number"}
-                      </div>
-                    )}
-                  </ErrorMessage>
-                </div>
-              </div>
-              <label>Password</label>
-              <div style={{ marginBottom: "20px" }}>
-                <Field name="password" type="password" />
+              <div className="ui segment">
+                {" "}
+                <h1>Login</h1>
+                <label>Email</label>
                 <div>
-                  <ErrorMessage name="password" />
+                  <Field name="email" type="email" />
+                  <div style={{ marginBottom: "20px" }}>
+                    <ErrorMessage name="email" />
+                  </div>
                 </div>
+                <label>Phone no.</label>
+                <div>
+                  <Field name="phone" />
+                  <div style={{ marginBottom: "20px" }}>
+                    <ErrorMessage name="phone">
+                      {(msg) => (
+                        <div>
+                          {msg === "Required" ? <div>"Required"</div> : "Phone must be number"}
+                        </div>
+                      )}
+                    </ErrorMessage>
+                  </div>
+                </div>
+                <label>Password</label>
+                <div style={{ marginBottom: "20px" }}>
+                  <Field name="password" type="password" />
+                  <div>
+                    <ErrorMessage name="password" />
+                  </div>
+                </div>
+                <div style={{ marginBottom: "20px" }}>
+                  <Field name="select" as={Dropdown} />
+                  <div>
+                    <ErrorMessage name="select" />
+                  </div>
+                </div>
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+                <div>{message}</div>
               </div>
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-              <div>{message}</div>
             </Form>
           );
         }}
